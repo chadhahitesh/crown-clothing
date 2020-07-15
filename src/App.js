faa80964c9b5f6e0542847bcfcb79c213d80c2a1
Hitesh <chadha.hitesh@gmail.com>
2020-07-15 23:37:18 +0530

    Implement Auth and SIgn form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,10 +18,21 @@ class App extends React.Component {
   
   unsubscribeFromAuth = null
   componentDidMount() {
-    this.unsubscribeFromAuth=auth.onAuthStateChanged(user =>{
-      this.setState({currentUser:user});
+    this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth =>{
+     if(userAuth){
+       const userRef =await createUserProfileDocument(userAuth);
 
-      console.log(user)
+       userRef.onSnapshot(snapshot =>{
+         this.setState({
+           currentUser:{
+             id:snapshot.id,
+             ...snapshot.data()
+           }
+         })
+       }) ; 
+      
+     }
+     this.setState({currentUser:userAuth});
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,26 +1,50 @@
-import firebase from 'firebase/app';
-import'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-   
-        apiKey: "AIzaSyDglpcd7vkANN8qNEj2tf3yK2lHb3JO0O4",
-        authDomain: "crown-clothing-b6cf2.firebaseapp.com",
-        databaseURL: "https://crown-clothing-b6cf2.firebaseio.com",
-        projectId: "crown-clothing-b6cf2",
-        storageBucket: "crown-clothing-b6cf2.appspot.com",
-        messagingSenderId: "1010966774980",
-        appId: "1:1010966774980:web:e0b76755d7e7738bf3f591",
-        measurementId: "G-P576W18JL9"
-    };
+  apiKey: "AIzaSyDglpcd7vkANN8qNEj2tf3yK2lHb3JO0O4",
+  authDomain: "crown-clothing-b6cf2.firebaseapp.com",
+  databaseURL: "https://crown-clothing-b6cf2.firebaseio.com",
+  projectId: "crown-clothing-b6cf2",
+  storageBucket: "crown-clothing-b6cf2.appspot.com",
+  messagingSenderId: "1010966774980",
+  appId: "1:1010966774980:web:e0b76755d7e7738bf3f591",
+  measurementId: "G-P576W18JL9",
+};
 
-    firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+ if (!userAuth) return;
 
-    export const auth = firebase.auth();
-    export const firestore = firebase.firestore();
+ const userRef = firestore.doc(`users/${userAuth.uid}`)
 
-    const provider = new firebase.auth.GoogleAuthProvider();
-    provider.setCustomParameters({prompt:'select_account'});
-    export const signInWithGoogle = () => auth.signInWithPopup(provider);
+ const snapShot = await userRef.get();
 
-    export default firebase;
+ if (!snapShot.exists){
+   const {displayName ,email} =userAuth;
+   const  createdAt = new Date();
+
+   try{
+  await userRef.set ({
+      displayName,
+      email,
+      createdAt,
+      ...additionalData
+  })
+   } catch (error){
+   console.log('error creating user' , error.message);
+   }
+ }
+ return userRef
+
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
