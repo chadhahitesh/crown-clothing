78ea74ac6a517fd7a6e0222f0c5ffd408ed8f6f7
Hitesh <chadha.hitesh@gmail.com>
2020-07-21 12:53:59 +0530

    Cart Component and its Reducer added with working dropdown feature

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import {auth} from '../../firebase/firebase.utils';
-import {connect} from 'react-redux'
+import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser,hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -17,21 +19,27 @@ const Header = ({currentUser}) => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
-      {
-        currentUser ? (
-          <div className='option' onClick={()=> auth.signOut()}>
-            SIGN OUT
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
-        ) : (
-          <Link className="option" to="/signin">
-        SIGN IN
-      </Link>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
       )}
+      <CartIcon />
     </div>
+    {
+      hidden ? null :
+      <CartDropdown />
+    }
+    
   </div>
 );
 
-const mapStateToProps = state =>({
-  currentUser : state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+  currentUser,
+  hidden
 });
-export default connect (mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header);
